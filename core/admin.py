2fca9337097e5f3c5154b5526a1719c64180d97e
Fernando <fernandoalvvj@gmail.com>
2024-11-21 03:18:02 -0600

    cambiar idioma a español y admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import AmortizationTable, SinkingFund
 
-# Register your models here.
+@admin.register(AmortizationTable)
+class AmortizationTableAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at')
+    list_filter = ('user', 'created_at')
+    search_fields = ('user__username',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+@admin.register(SinkingFund)
+class SinkingFundAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at')
+    list_filter = ('user', 'created_at')
+    search_fields = ('user__username',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
